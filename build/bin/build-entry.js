5896d15de571845e3ccd9af945f6a2c7c5ab2e33
Cr <631807682@qq.com>
2020-04-20 16:30:45 +0800

    build: auto generate index

---
+++ build/bin/build-entry.js
@@ -0,0 +1,60 @@
+const fs = require('fs')
+const render = require('json-templater/string')
+const uppercamelcase = require('uppercamelcase')
+const path = require('path')
+const endOfLine = require('os').EOL
+const startOfLine = '  '.repeat(1)
+
+const OUTPUT_PATH = path.join(__dirname, '../../src/index.ts')
+const backlist = ['theme-chalk']
+
+let packageNames = fs.readdirSync('./packages', 'utf-8')
+let components = packageNames
+  .filter((pn) => !backlist.includes(pn))
+  .map((pn) => ({
+    name: uppercamelcase(pn),
+    package: pn,
+  }))
+
+const importComponents = components.map(({ name, package }) => {
+  return `import { install as ${name} } from 'packages/${package}'`
+})
+
+const exportComponents = components.map(({ name, package }) => {
+  return `export * from 'packages/${package}'`
+})
+
+const template = `
+import { App } from 'vue'
+import { ElementUIOptions } from 'src/component'
+{{importComponents}}
+
+const components = [
+  {{components}}
+]
+
+export const install = function (app: App, opts = {}) {
+  components.forEach((comp)=>{
+    app.use(comp)
+  })
+
+  ElementUIOptions.value = {
+    ...ElementUIOptions.value,
+    ...opts,
+  }
+}
+
+export const version = '{{version}}'
+export * from 'src/component'
+{{exportComponents}}
+`
+
+const code = render(template, {
+  version: require('../../package.json').version,
+  components: components.map((c) => c.name).join(',' + endOfLine + startOfLine),
+  importComponents: importComponents.join(endOfLine),
+  exportComponents: exportComponents.join(endOfLine),
+})
+
+fs.writeFileSync(OUTPUT_PATH, code)
+// console.log('[build entry] DONE:', code)
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,28 @@
-import { App } from "vue";
-import { install as Button } from "packages/button";
-import { install as ButtonGroup } from "packages/button-group";
-import { install as Icon } from "packages/icon";
-import { install as Input } from "packages/input";
-import { ElementUIOptions } from "src/component";
+import { App } from 'vue'
+import { ElementUIOptions } from 'src/component'
+import { install as Button } from 'packages/button'
+import { install as ButtonGroup } from 'packages/button-group'
+import { install as Icon } from 'packages/icon'
+import { install as Input } from 'packages/input'
+import { install as InputNumber } from 'packages/input-number'
+
+const components = [Button, ButtonGroup, Icon, Input, InputNumber]
 
 export const install = function (app: App, opts = {}) {
-  app.use(Button);
-  app.use(ButtonGroup);
-  app.use(Icon);
-  app.use(Input);
+  components.forEach((comp) => {
+    app.use(comp)
+  })
 
   ElementUIOptions.value = {
     ...ElementUIOptions.value,
     ...opts,
-  };
-};
+  }
+}
 
-export * from "src/component";
-export * from "packages/button";
-export * from "packages/button-group";
-export * from "packages/icon";
-export * from "packages/input";
+export const version = 'v0.0.0-alpha.0'
+export * from 'src/component'
+export * from 'packages/button'
+export * from 'packages/button-group'
+export * from 'packages/icon'
+export * from 'packages/input'
+export * from 'packages/input-number'
