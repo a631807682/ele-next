7a0e8010acf448a515bbc58159b5423fd31d8ec0
Cr <631807682@qq.com>
2020-07-02 16:12:46 +0800

    test: fix backtop test

--- test/unit/specs/backtop.spec.ts
+++ test/unit/specs/backtop.spec.ts
@@ -2,23 +2,33 @@ import { mountWithElement, wait } from '../util'
 
 describe('Backtop', () => {
   it('create', async () => {
-    const wrapper = mountWithElement({
-      template: `
-      <div ref="scrollTarget" class="test-scroll" 
-        style="height: 100px; overflow: auto">
+    const outerNode = document.createElement('div')
+    outerNode.setAttribute('class', 'test-scroll')
+    outerNode.setAttribute('style', 'height: 100px; overflow: auto')
+
+    document.body.appendChild(outerNode)
+
+    const wrapper = mountWithElement(
+      {
+        template: `
         <div style="height: 10000px; width: 100%">
           <el-backtop target=".test-scroll">
             <span>test_up_text</span>
           </el-backtop>
         </div>
-      </div>
       `,
-    })
+      },
+      {
+        attachTo: outerNode,
+      }
+    )
+
     expect(wrapper.vm.$el).toBeTruthy()
     expect(wrapper.text()).toEqual('')
-    const el = wrapper.vm.$refs.scrollTarget as any
-    el.scrollTop = 2000
-    await wait()
-    expect(wrapper.find('.el-backtop').text()).toEqual('test_up_text')
+    outerNode.scrollTop = 2000
+    outerNode.dispatchEvent(new Event('scroll'))
+
+    await wait(300)
+    expect(wrapper.text()).toEqual('test_up_text')
   })
 })
